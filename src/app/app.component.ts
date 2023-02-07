1be9ed90aedeec4f9c7d051ac6a3668621e95573
Rodrigo Pimenta Dias <rodrigo.pimenta@fengbrasil.com.br>
2023-02-06 23:00:56 -0300

    custom page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { WindowRef } from './WindowRef';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-quickstart';
+  title = 'Luana Luz';
+
+
+  constructor(private winRef: WindowRef) {
+    // getting the native window obj
+    console.log('Native window obj', winRef.nativeWindow);
+  }
+
+  sendMessage() {
+    console.log({ from: 'sendMessage' })
+    this.winRef.nativeWindow.botpressWebChat.sendEvent({ type: "message", text: "Olá..." })
+    // this.winRef.nativeWindow.botpressWebChat.sendEvent({
+    //   type: "proactive-trigger",
+    //   channel: "web",
+    //   payload: {
+    //     text: "fake message",
+    //   },
+    // })
+    // this.winRef.nativeWindow.botpressWebChat.sendEvent({
+    //   type: 'proactive-trigger',
+    //   channel: 'web',
+    //   payload: {
+    //     text: 'Hi, I am Pro active'
+    //   }
+    // })
+  }
 }
